37964a86a469daa5bad9a20c5d739ba02ecf6f3e
Ookiineko <chiisaineko@protonmail.com>
2022-07-30 22:13:42 +0900

    drivers: input: input: Port Xiaomi changes from dandelion-q-oss
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -2,6 +2,7 @@
  * The input core
  *
  * Copyright (c) 1999-2002 Vojtech Pavlik
+ * Copyright (C) 2021 XiaoMi, Inc.
  */
 
 /*
@@ -874,9 +875,9 @@ static int input_default_setkeycode(struct input_dev *dev,
 				break;
 			}
 		}
+		__set_bit(ke->keycode, dev->keybit);
 	}
 
-	__set_bit(ke->keycode, dev->keybit);
 	return 0;
 }
 
@@ -932,11 +933,7 @@ int input_set_keycode(struct input_dev *dev,
 	 * Simulate keyup event if keycode is not present
 	 * in the keymap anymore
 	 */
-	if (old_keycode > KEY_MAX) {
-		dev_warn(dev->dev.parent ?: &dev->dev,
-			 "%s: got too big old keycode %#x\n",
-			 __func__, old_keycode);
-	} else if (test_bit(EV_KEY, dev->evbit) &&
+	if (test_bit(EV_KEY, dev->evbit) &&
 		   !is_event_supported(old_keycode, dev->keybit, KEY_MAX) &&
 		   __test_and_clear_bit(old_keycode, dev->key)) {
 		struct input_value vals[] =  {
