57d4a95af113428ff16666b7a0e35dc5870c4378
Ookiineko <chiisaineko@protonmail.com>
2022-07-30 21:09:16 +0900

    drivers: firmware_loader: Port Xiaomi changes from dandelion-q-oss
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- drivers/base/firmware_loader/main.c
+++ drivers/base/firmware_loader/main.c
@@ -3,6 +3,7 @@
  * main.c - Multi purpose firmware loading support
  *
  * Copyright (c) 2003 Manuel Estrada Sainz
+ * Copyright (C) 2021 XiaoMi, Inc.
  *
  * Please see Documentation/firmware_class/ for more information.
  *
@@ -278,6 +279,7 @@ static void free_fw_priv(struct fw_priv *fw_priv)
 static char fw_path_para[256];
 static const char * const fw_path[] = {
 	fw_path_para,
+	"/vendor/firmware",
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
