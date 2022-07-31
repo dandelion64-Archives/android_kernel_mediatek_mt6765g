b974ba899697ecd2f79f7d9229a5481d9ed82bc1
Ookiineko <chiisaineko@protonmail.com>
2022-07-31 16:47:21 +0900

    mtk-drv: flashlight: Port Silergy SY7803 from dandelion-q-oss
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- drivers/misc/mediatek/flashlight/flashlight-device.c
+++ drivers/misc/mediatek/flashlight/flashlight-device.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (c) 2019 MediaTek Inc.
+ * Copyright (C) 2021 XiaoMi, Inc.
  */
 
 #include "flashlight-core.h"
@@ -70,6 +71,11 @@ const struct flashlight_device_id flashlight_id[] = {
 	/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
 	{0, 0, 0, "flashlights-mt6370", 0, 0},
 	{0, 1, 0, "flashlights-mt6370", 1, 0},
+};
+#elif defined(mt6765)
+const struct flashlight_device_id flashlight_id[] = {
+	/* {TYPE, CT, PART, "NAME", CHANNEL, DECOUPLE} */
+	{0, 0, 0, "flashlights-sy7803", 0, 1},
 };
 #elif defined(mt6799)
 const struct flashlight_device_id flashlight_id[] = {
